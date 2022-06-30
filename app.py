7bd36cf386092f445c959a916b8f3716f36ec994
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-06-30 15:44:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,8 @@ data1_unique = pd.read_csv('facturas unicas.csv',sep=",")
 row2_1, row2_2, row2_3, row2_4 = st.columns((1,2,1,1))
 
 with row2_1:
-        # a = st.radio('Gender',('M','F'))
-    row2_1_1, row2_1_2 = st.columns((1,1))
-    with row2_1_1:
-        Female = st.checkbox('FEMALE')
-    with row2_1_2:
-        Male = st.checkbox('MALE')
+    Female = st.checkbox('FEMALE')
+    Male = st.checkbox('MALE')
     if (Female is True and Male is True) or (Female is False and Male is False):
         st.subheader('TOTAL VISITS')
         PatientsAll = data1_unique['numero de identificacion del paciente'].nunique()
