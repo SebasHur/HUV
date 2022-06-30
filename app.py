64bdadff789ff4dd53a8a0568c7ea956393a806c
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-06-30 11:44:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ img1 = Image.open('logo2.png')
 st.set_page_config(page_title='Hospital Universitario',page_icon = img1, layout='wide',initial_sidebar_state='collapsed')
 
 #Titulo 
-stc.html("""<table style="background: rgb(47, 84, 150); border-collapse: collapse; border: none; margin-right: calc(2%); width: 98%;">
-                <tbody>
-                    <tr>
-                        <td style="width: 450.8pt;border: 1pt solid windowtext;padding: 0cm 5.4pt;vertical-align: top;">
-                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><strong><span style="font-size:19px;color:#5B9BD5;">HOSPITAL UNIVERSITARIO DEL VALLE</span></strong></p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>""")    
+stc.html("""<table style="background: rgb(47, 84, 150); border-collapse: collapse; border: none; margin-right: calc(3%); width: 97%;">
+            <tbody>
+                <tr>
+                    <td style="width: 450.8pt;border: 1pt solid windowtext;background: rgb(142, 170, 219);padding: 0cm 5.4pt;vertical-align: top;">
+                        <p style="text-align: center;"><span style='font-size: 24px; font-family: "Arial Black", sans-serif; color: white;'><strong>HOSPITAL UNIVERSITARIO DEL VALLE</strong></span></p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>""")
 
 #Select/Multiple Select
 
