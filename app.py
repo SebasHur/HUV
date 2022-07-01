0d0fcbe8e04d39f48120f8c7117a5cb3a6bc7c11
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-01 10:26:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -112,9 +112,9 @@ elif choice == 'EDA':
         Female = st.checkbox('FEMALE')
         Male = st.checkbox('MALE')
         if Female is True:
-            Male.value = False
+            Male.checked = False
         if Male is True:
-            Female.value = False
+            Female.checked = False
     with row2_2:
         stc.html('''<table style="border-collapse:collapse;border:none;">
             <tbody>
