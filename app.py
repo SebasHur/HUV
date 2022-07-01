66c4a158792435440b851a47fe1b4e1a176522a0
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-01 14:44:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ elif choice == 'EDA':
         stc.html('''<table style="border-collapse:collapse;border:none;">
             <tbody>
                 <tr>
-                    <td style="width: 450.8pt; border: 1pt solid windowtext; background: rgb(97, 189, 109); padding: 0cm 5.4pt; vertical-align: middle; text-align: justify;">
+                    <td style="width: 100%; border-collapse: collapse; background: rgb(97, 189, 109); padding: 0cm 5.4pt; vertical-align: middle; text-align: justify;">
                         <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><span style="font-size:27px;color:black;">SELECT GENDER</span></p>
                     </td>
                 </tr>
