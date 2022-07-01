29db8854146f3eceba4fbe4cd5a3486dc3304db6
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-01 18:52:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ elif choice == 'EDA':
             <tbody>
                 <tr>
                     <td style="width: 1450.8pt; border-collapse: collapse; background: rgb(97, 189, 109); padding: 0cm 5.4pt; vertical-align: middle; text-align: justify;">
-                        <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><span style="font-size:27px;color:white">SELECT GENDER</span></p>
+                        <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:normal;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><span style="color:white">SELECT GENDER</span></p>
                     </td>
                 </tr>
             </tbody>
