1c6316053ca9d76e04a02bbc3b808914e2a25a40
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-01 19:54:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -325,9 +325,9 @@ elif choice == 'EDA':
             </table>''',height=50)
         #multiple selections
         diagnosticos = data1_unique['cie10 egrdin'].to_list()
-        diagnoses5= st.multiselect('Select your diagnoses', diagnosticos, default='TODOS')
+        my_spoken = st.multiselect('Select your diagnoses', diagnosticos, default='TODOS')
         row5_1,row5_2,row5_3 = st.columns(3)
-        st.write(diagnoses5)
+        st.write(my_spoken)
 
 elif EDA_OPT == 'EPS':
         pass
