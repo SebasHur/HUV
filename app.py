d1abe00ee8d967dd44b4696edc92c96b71d340b5
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-03 14:06:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -497,12 +497,12 @@ elif choice == 'EDA':
                         </tbody>
                     </table>
                     <p><br></p>''',height=50)
-        top_EPS = st.slider('',min_value=3, max_value=33, value=10, step=1)
+        # top_EPS = st.slider('',min_value=3, max_value=33, value=10, step=1)
         row2_1, row2_2 = st.columns((2, 1))
         EPS_percent = data1_unique['responsable EPS'].value_counts().rename_axis('EPS').reset_index(name='PATIENTS')
         EPS_percent['PARTICIPATION'] = round((EPS_percent['PATIENTS']/EPS_percent['PATIENTS'].sum())*100,2).astype(float)
         with row2_1:
-            fig_EPS_2_1 = go.Figure(go.Funnel(y = EPS_percent['EPS'].head(top_EPS),x = EPS_percent['PATIENTS'].head(top_EPS),textposition = "inside",textinfo = "label"))
+            fig_EPS_2_1 = go.Figure(go.Funnel(y = EPS_percent['EPS'].head(5),x = EPS_percent['PATIENTS'].head(5),textposition = "inside",textinfo = "label"))
             fig_EPS_2_1.update_yaxes(showticklabels=False)
             fig_EPS_2_1.update_layout(font_size=14)
             fig_EPS_2_1.update_layout(
@@ -510,7 +510,7 @@ elif choice == 'EDA':
                 paper_bgcolor="LightSteelBlue")
             st.plotly_chart(fig_EPS_2_1, use_container_width=True)
         with row2_2:
-            st.dataframe(EPS_percent.head(top_EPS))
+            st.dataframe(EPS_percent)
 
 
             
