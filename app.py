770895610ea64ab32e8fe2d542e1a0750ba81955
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-02 19:18:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -234,6 +234,7 @@ elif choice == 'EDA':
                 df_month["months"] = df_month["month"].dt.strftime('%m')
                 df_month["years"] = df_month["month"].dt.strftime('%y')
                 fig4_1 = px.line(df_month, x='months', y='PATIENTS', color='years',range_x=(0,11))
+                fig4_1.update_layout(margin=dict(l=20, r=20, t=20, b=20),paper_bgcolor="LightSteelBlue")
                 st.plotly_chart(fig4_1, use_container_width=True)
             elif Gender1 == 'FEMALE':
                 by_month1 = Mujeres['fecha ingreso'].dt.to_period('m').value_counts().sort_index()
@@ -243,6 +244,7 @@ elif choice == 'EDA':
                 df_month["months"] = df_month["month"].dt.strftime('%m')
                 df_month["years"] = df_month["month"].dt.strftime('%y')
                 fig4_1 = px.line(df_month, x='months', y='PATIENTS', color='years',range_x=(0,11))
+                fig4_1.update_layout(margin=dict(l=20, r=20, t=20, b=20),paper_bgcolor="LightSteelBlue")
                 st.plotly_chart(fig4_1, use_container_width=True)
             elif Gender1 == 'MALE':
                 by_month1 = Hombres['fecha ingreso'].dt.to_period('m').value_counts().sort_index()
@@ -252,6 +254,7 @@ elif choice == 'EDA':
                 df_month["months"] = df_month["month"].dt.strftime('%m')
                 df_month["years"] = df_month["month"].dt.strftime('%y')
                 fig4_1 = px.line(df_month, x='months', y='PATIENTS', color='years',range_x=(0,11))
+                fig4_1.update_layout(margin=dict(l=20, r=20, t=20, b=20),paper_bgcolor="LightSteelBlue")
                 st.plotly_chart(fig4_1, use_container_width=True)
         with row4_1:
             stc.html('''<table style="border-collapse:collapse;border:none;">
