a1a5ac8acb8d0fe06974c0379af6c4420d2a6b5b
SebasHur <sebas.hurtado.ochoa@gmail.com>
2022-07-03 15:27:07 -0500

    1

--- app.py
+++ app.py
@@ -59,16 +59,7 @@ count_freq =dict(data1_unique['responsable EPS'].value_counts())
 data1_unique['count_freq'] = data1_unique['responsable EPS']
 data1_unique['count_freq'] = data1_unique['count_freq'].map(count_freq)
 
-def Grouped_Age_gender():
-    Gender_Age = data1_unique[['genero - sexo','Age']]
-    age_groups = pd.cut(Gender_Age['Age'], bins=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,np.inf])
-    Grouped_Age_gender = pd.crosstab(age_groups, Gender_Age['genero - sexo']).reset_index()
-    Grouped_Age_gender['F'] = Grouped_Age_gender['F'] * -1
-    Grouped_Age_gender['Age'] = Grouped_Age_gender['Age'].astype(str)
-    Grouped_Age_gender['Age'] = Grouped_Age_gender['Age'].replace(',','-',regex=True)
-    Grouped_Age_gender['Age'] = Grouped_Age_gender['Age'].replace('\(','',regex=True)
-    Grouped_Age_gender['Age'] = Grouped_Age_gender['Age'].replace(']','',regex=True)
-    return Grouped_Age_gender
+
 #Sidebar Menu
 st.sidebar.image('logo-HU_Horizontal_Azul.png')
 menu = ['HOME','EDA','PREDICTION','ABOUT']
@@ -115,11 +106,12 @@ if choice == 'HOME':
                 height=700)
 elif choice == 'EDA':
     EDA_OPT = st.sidebar.radio('Select what you want to explore',('Patients and Gender','EPS'))
+    
     if EDA_OPT == 'Patients and Gender':
         run_patients_gender()
     
     elif EDA_OPT == 'EPS':
-        run_EDA_eps()
+        run_EDA_eps(data1_unique)
               
 
 
